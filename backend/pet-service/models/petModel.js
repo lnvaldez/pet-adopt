be0621b3b14eab60a08ab7367d0112ba7f5bc410
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-08 12:58:42 -0300

    'addPet' async function creates a new pet

--- backend/pet-service/models/petModel.js
+++ backend/pet-service/models/petModel.js
@@ -9,3 +9,19 @@ const db = mysql.createConnection({
   password: dbData.password,
   database: dbData.database,
 });
+
+exports.addPet = async function (name, type, age) {
+  try {
+    const [result] = await db
+      .promise()
+      .execute("INSERT INTO pets (name, type, age) VALUES (?, ?, ?)", [
+        name,
+        type,
+        age,
+      ]);
+
+    console.log("Succesfully added a new pet.");
+  } catch (error) {
+    console.error("Error adding new pet: ", error);
+  }
+};
