dfa43290a4c9be1ae4aba39502f7555f36d01eae
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-08 12:08:08 -0300

    Setup express server

--- backend/pet-service/server.js
+++ backend/pet-service/server.js
@@ -2,3 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv");
 
 dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 6001;
+
+app.listen(PORT, () => console.log(`Pet service running on port ${PORT}`));
