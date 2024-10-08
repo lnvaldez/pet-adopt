22475896d2f8514fec2f821cb59330626d75fb2f
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-08 13:32:43 -0300

    Specifying petRoutes path prefix

--- backend/pet-service/server.js
+++ backend/pet-service/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+const petRoutes = require("./routes/petRoutes");
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use("/pets", petRoutes);
 
 const PORT = process.env.PORT || 6001;
 
