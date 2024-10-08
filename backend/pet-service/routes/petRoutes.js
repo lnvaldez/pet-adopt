1e8c08a313d29b25a265840d29d7c80137dce78b
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-08 13:46:32 -0300

    Add router methods and functions to execute at call

--- backend/pet-service/routes/petRoutes.js
+++ backend/pet-service/routes/petRoutes.js
@@ -7,3 +7,13 @@ const {
   updatePetStatus,
   deletePet,
 } = require("../controllers/petController");
+
+const router = express.Router();
+
+router.post("/", addNewPet);
+router.get("/available", getAvailablePets);
+router.put("/", updatePetName);
+router.put("/status", updatePetStatus);
+router.delete("/", deletePet);
+
+module.exports = router;
