75a60f90a4b6270443801604ffab392cc47fba50
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-08 13:25:13 -0300

    'addNewPet' async function creates new pet through req body data

--- backend/pet-service/controllers/petController.js
+++ backend/pet-service/controllers/petController.js
@@ -1,1 +1,12 @@
 const Pet = require("../models/petModel");
+
+exports.addNewPet = async (req, res) => {
+  const { name, type, age } = req.body;
+
+  try {
+    await Pet.addPet(name, type, age);
+    res.status(201).json({ message: "Pet added." });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to add pet." });
+  }
+};
