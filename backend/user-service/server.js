658d53d289f1832f3ef202c2591ac56f0d58bef8
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-09 13:35:45 -0300

    Setup express server for user service

---
+++ backend/user-service/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+const userRoutes = require("./routes/userRoutes");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use("/user", userRoutes);
+
+const PORT = process.env.DB_PORT || 6000;
+
+if (require.main === module) {
+  app.listen(PORT, () => console.log(`User service running at port ${PORT}`));
+} else {
+  module.exports = app;
+}
