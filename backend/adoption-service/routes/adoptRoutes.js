cf725d4716d20b3f9f948ba65a912dc6e4cb79f1
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-10 12:34:50 -0300

    Add token authentication to adoption routes

--- backend/adoption-service/routes/adoptRoutes.js
+++ backend/adoption-service/routes/adoptRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
 const adoptPet = require("../controllers/adoptController");
+const { authenticateToken } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.post("/", adoptPet);
+router.post("/", authenticateToken, adoptPet);
 
 module.exports = router;
