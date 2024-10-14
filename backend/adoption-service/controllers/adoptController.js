9bd2bca42d2c5cf91738db9a8f6cd5c48e23bed4
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-14 09:32:36 -0300

    Implement circuit breaker

--- backend/adoption-service/controllers/adoptController.js
+++ backend/adoption-service/controllers/adoptController.js
@@ -1,18 +1,35 @@
 const Adopt = require("../models/adoptModel");
 const rabbitmq = require("../../shared/rabbitmq/publishMessage");
+const CircuitBreaker = require("opossum");
 
 const rabbitmqUrl = "amqp://localhost:5672";
 
 exports.adoptPet = async (req, res) => {
   const { petId, adopterId } = req.body;
 
-  const message = { petId: petId, adopterId: adopterId };
+  const message = { petId, adopterId };
+
+  const options = {
+    timeout: 3000,
+    errorThresholdPercentage: 50,
+    resetTimeout: 5000,
+  };
+
+  const publishMessageWithBreaker = new CircuitBreaker(
+    rabbitmq.publishMessage,
+    options
+  );
 
   try {
     await Adopt.adoptPet(petId, adopterId);
-    await rabbitmq.publishMessage("pet_adopted", message, rabbitmqUrl);
-    await res.status(201).json({ message: "Adoption succesful." });
+
+    await publishMessageWithBreaker.fire("pet_adopted", message, rabbitmqUrl);
+
+    res.status(201).json({ message: "Adoption successful." });
   } catch (error) {
+    if (publishMessageWithBreaker.opened) {
+      console.error("Circuit is open, too many failures:", error);
+    }
     res.status(500).json({ error: "Failed to adopt a pet." });
   }
 };
