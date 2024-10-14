060f070055007a3be2e370c535bdf853a7b42841
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-14 09:36:53 -0300

    Cleanup circuit breaker logic from adoption service controller

--- backend/adoption-service/controllers/adoptController.js
+++ backend/adoption-service/controllers/adoptController.js
@@ -1,6 +1,6 @@
 const Adopt = require("../models/adoptModel");
 const rabbitmq = require("../../shared/rabbitmq/publishMessage");
-const CircuitBreaker = require("opossum");
+const createCircuitBreaker = require("../../shared/circuit-breaker/circuitBreaker");
 
 const rabbitmqUrl = "amqp://localhost:5672";
 
@@ -9,15 +9,8 @@ exports.adoptPet = async (req, res) => {
 
   const message = { petId, adopterId };
 
-  const options = {
-    timeout: 3000,
-    errorThresholdPercentage: 50,
-    resetTimeout: 5000,
-  };
-
-  const publishMessageWithBreaker = new CircuitBreaker(
-    rabbitmq.publishMessage,
-    options
+  const publishMessageWithBreaker = createCircuitBreaker(
+    rabbitmq.publishMessage
   );
 
   try {
