34e40fed00e6a1cbdd7a4c0af64cc7c012e827ad
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-14 09:56:57 -0300

    Improve logging for adoptController.js

--- backend/adoption-service/controllers/adoptController.js
+++ backend/adoption-service/controllers/adoptController.js
@@ -8,20 +8,24 @@ exports.adoptPet = async (req, res) => {
   const { petId, adopterId } = req.body;
 
   const message = { petId, adopterId };
-
   const publishMessageWithBreaker = createCircuitBreaker(
     rabbitmq.publishMessage
   );
 
   try {
     await Adopt.adoptPet(petId, adopterId);
-
     await publishMessageWithBreaker.fire("pet_adopted", message, rabbitmqUrl);
 
     res.status(201).json({ message: "Adoption successful." });
+    console.log(`Adoption successful: petId=${petId}, adopterId=${adopterId}`);
   } catch (error) {
     if (publishMessageWithBreaker.opened) {
-      console.error("Circuit is open, too many failures:", error);
+      console.error(
+        "Circuit breaker is open, too many failures:",
+        error.message
+      );
+    } else {
+      console.error("Error adopting pet:", error.message);
     }
     res.status(500).json({ error: "Failed to adopt a pet." });
   }
