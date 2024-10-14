3218df51e4c130c75fdfc91d4633128b51e78a33
Lucas Valdez <lnvaldez24@gmail.com>
2024-10-14 09:50:03 -0300

    Improve petModel.js throw new Error consistency

--- backend/pet-service/models/petModel.js
+++ backend/pet-service/models/petModel.js
@@ -10,79 +10,77 @@ const pool = mysql.createPool({
   database: dbData.database,
 });
 
-exports.addPet = async function (name, type, age) {
+exports.addPet = async (name, type, age) => {
   try {
     await pool.execute("INSERT INTO pets (name, type, age) VALUES (?, ?, ?)", [
       name,
       type,
       age,
     ]);
-
-    console.log("Succesfully added a new pet.");
+    console.log(
+      `Successfully added new pet: name=${name}, type=${type}, age=${age}`
+    );
   } catch (error) {
-    console.error("Error adding new pet: ", error);
-    throw error;
+    console.error("Error adding new pet:", error.message);
+    throw new Error("Failed to add pet.");
   }
 };
 
-exports.getPets = async function () {
+exports.getPets = async () => {
   try {
     const [result] = await pool.execute(
       "SELECT name, type, age, description FROM pets WHERE status = 'Available'"
     );
-
-    console.log("Fetched all pets from database.");
+    console.log("Fetched all available pets.");
     return result;
   } catch (error) {
-    console.error("Error fetching all pets: ", error);
-    throw error;
+    console.error("Error fetching pets:", error.message);
+    throw new Error("Failed to fetch pets.");
   }
 };
 
-exports.updatePet = async function (newName, name) {
+exports.updatePet = async (newName, name) => {
   try {
     const [result] = await pool.execute(
       "UPDATE pets SET name = ? WHERE name = ? LIMIT 1",
       [newName, name]
     );
+    if (result.affectedRows === 0) throw new Error("Pet not found.");
 
-    if (result.affectedRows === 0) {
-      throw new Error("Pet not found.");
-    }
-
-    console.log(`Updated pet ${name}'s to ${newName}.`);
+    console.log(
+      `Successfully updated pet: oldName=${name}, newName=${newName}`
+    );
   } catch (error) {
-    console.error("Error updating pet name: ", error);
-    throw error;
+    console.error("Error updating pet:", error.message);
+    throw new Error("Failed to update pet.");
   }
 };
 
-exports.updatePetStatus = async function (id) {
+exports.updatePetStatus = async (id) => {
   try {
     const [result] = await pool.execute(
       "UPDATE pets SET status = 'Adopted' WHERE id = ?",
       [id]
     );
+    if (result.affectedRows === 0) throw new Error("Pet not found.");
 
-    if (result.affectedRows === 0) {
-      throw new Error("Pet not found.");
-    }
+    console.log(`Updated pet status to 'Adopted': id=${id}`);
   } catch (error) {
-    console.error("Error updating pet status: ", error);
+    console.error("Error updating pet status:", error.message);
+    throw new Error("Failed to update pet status.");
   }
 };
 
-exports.deletePet = async function (name) {
+exports.deletePet = async (name) => {
   try {
     const [result] = await pool.execute("DELETE FROM pets WHERE name = ?", [
       name,
     ]);
+    if (result.affectedRows === 0) throw new Error("Pet not found.");
 
-    if (result.affectedRows === 0) {
-      throw new Error("Pet not found.");
-    }
+    console.log(`Successfully deleted pet: name=${name}`);
   } catch (error) {
-    console.error("Error deleting pet: ", error);
-    throw error;
+    console.error("Error deleting pet:", error.message);
+    throw new Error("Failed to delete pet.");
   }
 };
